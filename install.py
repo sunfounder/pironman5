72be258361c2dea62e3e03b9de9c17dc0692d3cc
Cavon <cavonxx@gmail.com>
2025-03-17 09:53:04 +0800

    update pm_auto

--- install.py
+++ install.py
@@ -32,7 +32,7 @@ settings = {
     # - Install python source code from git
     'python_source': {
         'pironman5': './',
-        'pm_auto': 'git+https://github.com/sunfounder/pm_auto.git@1.2.2',
+        'pm_auto': 'git+https://github.com/sunfounder/pm_auto.git@1.2.4',
     },
 
     # - Setup config txt
