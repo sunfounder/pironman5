3871b4035a41f2e74d4978438199eecd58c45cb8
Cavon <cavonxx@gmail.com>
2025-07-16 16:20:48 +0800

    build: update pipower5 driver and email templates to v1.2.0
    Update download URLs for both driver and email templates to use version 1.2.0 instead of 1.1.0 to ensure compatibility with the latest features and bug fixes.

--- scripts/setup_pipower5.sh
+++ scripts/setup_pipower5.sh
@@ -7,7 +7,7 @@ apt-get install wget unzip
 
 echo "Installing PiPower 5 driver"
 
-wget https://github.com/sunfounder/pipower5/releases/download/1.1.0/driver.zip
+wget https://github.com/sunfounder/pipower5/releases/download/1.2.0/driver.zip
 unzip driver.zip
 cd driver
 bash install.sh
@@ -16,7 +16,7 @@ rm -rf driver.zip driver/
 
 echo "Setting up email templates"
 
-wget https://github.com/sunfounder/pipower5/releases/download/1.1.0/email_templates.zip
+wget https://github.com/sunfounder/pipower5/releases/download/1.2.0/email_templates.zip
 unzip email_templates.zip
 cp -r email_templates/ /opt/pipower5
 rm -rf email_templates.zip email_templates/
